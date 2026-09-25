41d6e44a16120ae14fdb14446ed46a2a6fd79776
HexveilWorks <rhea.petkar25@gmail.com>
2026-09-25 17:42:00 +0530

    Update variable.tf

--- Dev/variable.tf
+++ Dev/variable.tf
@@ -17,3 +17,21 @@ variable "aks" {
     dns_prefix = string
   }))
 }
+
+variable "vnet"{
+type=map(object{(
+vnet_name=string
+rg_name=string
+location=string
+address_space=list(string)
+)})
+}
+
+variable "subnet"{
+type=map(object{(
+subnet_name=string
+vnet_name=string
+rg_name=string
+address_prefixes=list(string)
+)})
+}
