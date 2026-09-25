47e77a99cbef6e93667271315f87897d04d1acfe
HexveilWorks <rhea.petkar25@gmail.com>
2026-09-25 17:35:34 +0530

    Update variable.tf

--- Modul/vnet/variable.tf
+++ Modul/vnet/variable.tf
@@ -1,1 +1,18 @@
 
+variable "vnet"{
+type=map(object{(
+vnet_name=string
+rg_name=string
+location=string
+address_space=list(string)
+)})
+}
+
+variable "subnet"{
+type=map(object{(
+subnet_name=string
+vnet_name=string
+rg_name=string
+address_prefixes=list(string)
+)})
+}
